70579278ff0336ab7fb549166e2ff08b8fa6cc52
Mark16688 <118809002+Mark16688@users.noreply.github.com>
2022-11-22 16:46:10 +0700

    Add files via upload

---
+++ TheTetrisGame/src/main/java/com/mycompany/thetetrisgame/Tetris.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.thetetrisgame;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author mamapapadevstudio
+ */
+public class Tetris extends JFrame {
+
+    private JLabel statusbar;
+
+    public Tetris() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+
+        var board = new Board(this);
+        add(board);
+        board.start();
+
+        setTitle("Tetris");
+        setSize(300, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    JLabel getStatusBar() {
+
+        return statusbar;
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+
+            var game = new Tetris();
+            game.setVisible(true);
+        });
+    }
+}
